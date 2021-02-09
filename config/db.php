2ff2f593843aeff94852ac8431384e7d97fe3ef9
Zbyszek <57629429+ziggy83pl@users.noreply.github.com>
2021-02-09 17:12:31 +0100

    Delete db.php

--- config/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mysql_server = ""; //nazwa hostu
-$mysql_user = ""; //nazwa uzytkownika
-$mysql_pass = ""; //hasło do mysqul
-$mysql_db = ""; //nazwa bayz danych
-
-define("_DB_PREFIX_", "");
-
