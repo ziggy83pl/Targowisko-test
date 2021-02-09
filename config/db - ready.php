4ad275e521fd66a05bc04ee71ad0e3e32f1ab052
Zbyszek <57629429+ziggy83pl@users.noreply.github.com>
2021-02-09 17:12:42 +0100

    Delete db - ready.php

--- config/db - ready.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$mysql_server = ""; // nazwa serwera bazy danych
-$mysql_user = "";  // użytkownik bazy danych
-$mysql_pass = "";  // hasło do bazy danych
-$mysql_db = ""; // nazwa bazy danych
-
-define('_DB_PREFIX_', '');
-
